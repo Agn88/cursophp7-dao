0c7cf62f41f33d55723c6db3433132092461d9f8
unknown <agnaldo.junior@unoesc.edu.br>
2020-07-26 22:05:39 -0300

    Ajustado classe Usuario para Listagens

--- index.php
+++ index.php
@@ -10,11 +10,38 @@ $usuarios = $sql->select('SELECT * FROM tb_usuarios');
 echo json_encode($usuarios);
 */
 
+
+//Carrega um usuário
+/*
 $root = new Usuario();
 
 $root->loadById(7);
 
 echo $root;
+*/
 
+
+/*Carrega uma lista de usuario
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+*/
+
+
+/*Carrega uma lista de usuario buscando pelo login
+$search = Usuario::search("Ag");
+
+echo json_encode($search);
+*/
+
+
+//Carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login(
+    "Agnaldo",
+    "123453"
+);
+
+echo $usuario;
 
 ?>
